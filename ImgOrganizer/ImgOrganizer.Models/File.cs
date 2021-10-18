efc60806409640a612f91135d0a31688796c58dd
gecho <cholakovge@gmail.com>
2021-10-18 19:27:54 +0200

    File model

---
+++ ImgOrganizer/ImgOrganizer.Models/File.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImgOrganizer.Models
+{
+    public class File
+    {
+        public DateTime CreationTime { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string Directory { get; set; }
+    }
+}

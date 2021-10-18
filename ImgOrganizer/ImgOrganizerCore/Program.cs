3f0ea71ba30934cc9a8ccc01b563768de8d5ab15
gecho <cholakovge@gmail.com>
2021-10-18 19:24:29 +0200

    Read files from directory and get creation time

--- ImgOrganizer/ImgOrganizerCore/Program.cs
+++ ImgOrganizer/ImgOrganizerCore/Program.cs
@@ -8,8 +8,20 @@ namespace ImgOrganizerCore
     {
         static void Main(string[] args)
         {
-            //var docPath = Environment.SpecialFolder.Desktop;
-            //List<string>
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(docPath));
+            var targetDir = dirs.Find(x => x.Contains("Test"));
+            DirectoryInfo directoryInfo = new DirectoryInfo(targetDir);
+
+            var files = directoryInfo.EnumerateFiles();
+
+            foreach (var file in directoryInfo.EnumerateFiles())
+            {
+                Console.WriteLine(file.FullName);
+                var creationTime = file.CreationTimeUtc;
+                Console.WriteLine(creationTime);
+            }
+
         }
     }
 }

22a90213ed867e0adce8426dfd9f33f26d2b5187
gecho <cholakovge@gmail.com>
2021-10-18 12:47:05 +0200

    Project initial create

---
+++ ImgOrganizer/ImgOrganizerCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ImgOrganizerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var docPath = Environment.SpecialFolder.Desktop;
+            //List<string>
+        }
+    }
+}

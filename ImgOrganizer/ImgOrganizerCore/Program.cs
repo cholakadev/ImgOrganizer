78ef1de9388d78fabddf74d8849eb78decf9c7ce
gecho <cholakovge@gmail.com>
2021-10-18 23:32:16 +0200

    Organize files into folders by year and then by month

--- ImgOrganizer/ImgOrganizerCore/Program.cs
+++ ImgOrganizer/ImgOrganizerCore/Program.cs
@@ -14,20 +14,25 @@ namespace ImgOrganizerCore
             var targetDir = dirs.Find(x => x.Contains("Test"));
             DirectoryInfo di = new DirectoryInfo(targetDir);
 
-            foreach (var fl in di.EnumerateFiles())
+            foreach (var file in di.EnumerateFiles())
             {
-                var year = fl.CreationTimeUtc.Year;
-                var month = fl.CreationTimeUtc.Month;
+                var year = file.CreationTimeUtc.Year;
+                var month = file.CreationTimeUtc.Month;
                 var creationDate = new DateTime(year, month, 1);
 
                 // For each file create folder with the created year as name and the created month in the year folder.
+                // Create method for it
                 var directoryPathsByYearAndMonths = creationDate.ToString("yyyy/MM");
 
-                di.CreateSubdirectory(directoryPathsByYearAndMonths);
+                if (di.Exists)
+                {
+                    di.CreateSubdirectory(directoryPathsByYearAndMonths);
+                    //File.Move(targetDir, directoryPathsByYearAndMonths);
+                    var fileSourcePath = @$"{file.FullName}";
+                    var fileDestPath = @$"{targetDir}/{directoryPathsByYearAndMonths}/{file.Name}";
+                    File.Move(fileSourcePath, fileDestPath);
+                }
             }
-
-
-
         }
     }
 }
